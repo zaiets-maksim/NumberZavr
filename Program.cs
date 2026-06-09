09d3b06ad5e4f11ebc417335eb8bbfe94e7a28f5
zaiets-maksim <zaiets.maksim@gmail.com>
2026-06-09 21:07:24 +0300

    fix 2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
-using Microsoft.AspNetCore.Builder;
 using NumberZavr;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// services
-builder.Services.AddSingleton<GitHubStateService>();
+var config = builder.Configuration;
+
+var github = new GitHubStateService(
+    config["GitHubOwner"],
+    config["GitHubRepo"]
+);
+
+builder.Services.AddSingleton(github);
 builder.Services.AddSingleton<DataService>();
 builder.Services.AddHostedService<BotWorker>();
 
 var app = builder.Build();
 
-// health endpoint
 app.MapGet("/", () => "Bot is running");
 
 app.Run($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "10000"}");
